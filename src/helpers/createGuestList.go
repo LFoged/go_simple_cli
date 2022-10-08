a8d9c0121d01aef59ab8138a7794b4b727eb72c2
Louis <louis.foged@gmail.com>
2022-10-08 16:07:30 +0100

    refactor: change format of guest list to map[int]string, from map[int][]string

--- src/helpers/createGuestList.go
+++ src/helpers/createGuestList.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 )
 
-func CreateGuestList(bookings *map[int]models.Booking) map[int][]string {
-	guestList := make(map[int][]string)
+func CreateGuestList(bookings *map[int]models.Booking) map[int]string {
+	guestList := make(map[int]string)
 	for key, value := range *bookings {
-		guestList[key] = append(guestList[key], value.Name, fmt.Sprintf("%d tickets", len(value.Tickets)))
+		guestList[key] = fmt.Sprintf("%s | f%d tickets |", value.Name, len(value.Tickets))
 	}
 
 	return guestList
